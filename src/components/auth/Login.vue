47b6d5e319df961874cc27a0efcf5cd91dc312fc
pengchuanchao <gameboynes@gmail.com>
2020-08-05 17:09:16 +0800

    完成登陆页面设计

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,31 +1,38 @@
 <template>
-    <div class="login">
-        <div class="slide-in-elliptic-top-fwd">
-            <sui-form class="form">
-                <div class="welcome focus-in-contract-bck">
-                    <h3>NOMURA SMA</h3>
+    <div>
+        <div class="logo">
+            <img src="../../assets/images/logoNew.png" alt="">
+        </div>
+        <div class="login-wrapper">
+            <div class="login slide-in-elliptic-top-fwd">
+                <div class="">
+                    <sui-form class="form">
+                        <div class="welcome focus-in-contract-bck">
+                            <h1>SMA</h1>
+                        </div>
+                        <sui-form-field>
+                            <label><p class="label">用户</p></label>
+                            <label class="input">
+                                <input v-model="username" placeholder="请输入用户名" type="text" required/>
+                            </label>
+                        </sui-form-field>
+                        <sui-form-field>
+                            <label><p class="label">密码</p></label>
+                            <label class="input">
+                                <input v-model="password" placeholder="请输入密码" type="password" required/>
+                            </label>
+                        </sui-form-field>
+                        <sui-form-field style="color: white">
+                            <sui-checkbox label="7日免登陆" v-model="noResign" />
+                        </sui-form-field>
+                        <div is="sui-button-group" class="submit">
+                            <sui-button color="red" @click.prevent="login">登陆系统</sui-button>
+                            <sui-button-or />
+                            <sui-button @click.prevent="reset">修改密码</sui-button>
+                        </div>
+                    </sui-form>
                 </div>
-                <sui-form-field>
-                    <label>用户</label>
-                    <label class="input">
-                        <input v-model="username" placeholder="请输入用户名" type="text" required/>
-                    </label>
-                </sui-form-field>
-                <sui-form-field>
-                    <label>密码</label>
-                    <label class="input">
-                        <input v-model="password" placeholder="请输入密码" type="password" required/>
-                    </label>
-                </sui-form-field>
-                <sui-form-field>
-                    <sui-checkbox label="7日免登陆" v-model="noResign" />
-                </sui-form-field>
-                <div is="sui-button-group" class="submit">
-                    <sui-button color="red" @click.prevent="login">登陆系统</sui-button>
-                    <sui-button-or />
-                    <sui-button @click.prevent="reset">修改密码</sui-button>
-                </div>
-            </sui-form>
+            </div>
         </div>
     </div>
 </template>
@@ -53,6 +60,6 @@
     }
 </script>
 
-<style scoped lang="less">
+<style lang="less">
 @import "../../assets/style/login";
 </style>
