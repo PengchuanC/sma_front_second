dbb18ddf79cdb3b3b213deaf6cfaff4c7de5f296
pengchuanchao <gameboynes@gmail.com>
2020-09-28 16:20:44 +0800

    添加生产模式切换

--- src/common/variables.js
+++ src/common/variables.js
@@ -1,9 +1,31 @@
 // 常量或全局变量
 
+
+// 模式：production ｜ development ｜ test
+const MODE = Object.freeze({
+    production: Symbol(1),
+    development: Symbol(2),
+    test: Symbol(3)
+})
+
+const mode = MODE.development
+
 // 后端网址
-const baseUrl = "http://10.170.129.129/api/sma/"
-// const baseUrl = "http://0.0.0.0:8000/api/sma/"
-// const baseUrl = "http://10.170.236.56:8000/api/sma/"
+let baseUrl;
+switch (mode) {
+    case MODE.production:
+        baseUrl = "https://sma.nomuraoi-sec.com/api/sma/"
+        break
+    case MODE.development:
+        baseUrl = "http://10.170.129.129/api/sma/"
+        break
+    case MODE.test:
+        baseUrl = "http://0.0.0.0:8000/api/sma/"
+        break
+    default:
+        baseUrl = "http://10.170.129.129/api/sma/"
+}
+
 const chartColor = ['#900000','#C00000', '#D18484', '#E0B5B5', '#A6A6A6']
 
 
