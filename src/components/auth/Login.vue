97e22e77bdc4c2b6628999795e441730d88e1fa3
PengchuanC <31942935+PengchuanC@users.noreply.github.com>
2020-10-29 14:21:56 +0800

    修改登录背景

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -4,50 +4,51 @@
       <img src="../../assets/images/logoNew.png" alt="">
     </div>
     <div class="login-wrapper">
-      <div class="wrapper-mask"></div>
-      <div class="login slide-in-elliptic-top-fwd">
-        <div class="">
-          <sui-form class="form">
-            <div class="welcome focus-in-contract-bck">
-              <h2>欢迎访问野村东方SMA服务平台</h2>
-            </div>
-            <sui-form-field>
-              <label><p class="label">用户</p></label>
-              <label class="input">
-                <input v-model="username" placeholder="请输入用户名" type="text" required/>
-              </label>
-              <div class="remember-me">
-                <sui-checkbox v-model="remember" label="" style="color: white"/>
-                <p @click="remember=!remember">记住我的用户名</p>
+      <div class="wrapper-mask">
+        <div class="login slide-in-elliptic-top-fwd">
+          <div class="">
+            <sui-form class="form">
+              <div class="welcome focus-in-contract-bck">
+                <h3>欢迎访问野村东方国际证券SMA服务平台</h3>
               </div>
-            </sui-form-field>
-            <sui-form-field>
-              <label><p class="label">密码</p></label>
-              <label class="input">
-                <input v-model="password" placeholder="请输入密码" type="password" required/>
-              </label>
-            </sui-form-field>
-            <div is="sui-button-group" class="submit">
-              <sui-button color="red" @click.prevent="login">登陆系统</sui-button>
-              <sui-button-or/>
-              <sui-button @click.prevent="reset">修改密码</sui-button>
-            </div>
-          </sui-form>
-        </div>
-        <div class="forget">
-          <p class="item" @click="contactUs">忘记用户名或登陆密码</p>
-          <div class="info">
-            <a class="info-icon wechat">
-              <svg class="font-icon info-icon" aria-hidden="true">
-                <use xlink:href="#icongongzhonghaoguanli"></use>
-              </svg>
-            </a>
-            <div class="info-item">
-              <a class="info-icon legal" @click="toLegal">
+              <sui-form-field>
+                <label><p class="label">用户</p></label>
+                <label class="input">
+                  <input v-model="username" placeholder="请输入用户名" type="text" required/>
+                </label>
+                <div class="remember-me">
+                  <sui-checkbox v-model="remember" label="" style="color: white"/>
+                  <p @click="remember=!remember">记住我的用户名</p>
+                </div>
+              </sui-form-field>
+              <sui-form-field>
+                <label><p class="label">密码</p></label>
+                <label class="input">
+                  <input v-model="password" placeholder="请输入密码" type="password" required/>
+                </label>
+              </sui-form-field>
+              <div is="sui-button-group" class="submit">
+                <sui-button color="red" @click.prevent="login">登陆系统</sui-button>
+                <sui-button-or/>
+                <sui-button @click.prevent="reset">修改密码</sui-button>
+              </div>
+            </sui-form>
+          </div>
+          <div class="forget">
+            <p class="item" @click="contactUs">忘记用户名或登陆密码</p>
+            <div class="info">
+              <a class="info-icon wechat">
                 <svg class="font-icon info-icon" aria-hidden="true">
-                  <use xlink:href="#iconfalvshengmingfalv"></use>
+                  <use xlink:href="#icongongzhonghaoguanli"></use>
                 </svg>
               </a>
+              <div class="info-item">
+                <a class="info-icon legal" @click="toLegal">
+                  <svg class="font-icon info-icon" aria-hidden="true">
+                    <use xlink:href="#iconfalvshengmingfalv"></use>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
         </div>
